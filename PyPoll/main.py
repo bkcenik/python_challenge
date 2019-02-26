db46482533bd6767b6540bd4288bd19fc0100941
Bercin Cenik <bercin@gmail.com>
2019-02-25 18:25:23 -0600

    modified PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,18 +3,30 @@ import csv
 
 electionfile = os.path.join("resources", "election_data.csv")
 
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
 
+with open(electionfile, newline='') as csvfile:
+    print(csvfile) #tocheckpath
+    electionreader = csv.reader(csvfile, delimiter=',') #read in file
+    election_header = next(electionreader) #skip header
+    print(f"Header: {election_header}") #print the header to confirm
 
-with open(electionfile, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    csv_header = next(csvreader)
-    
-    vote = []
+    votes_cast = []
     candidate_list = []
-    candidate_vote = []
+    total_votes = []
+    splitter = ("----------------------------------------------------------")
 
-    for row in csvreader:
-        vote.append(csvreader[0])
-        totalvote = len(vote)
-        print(totalvote)
+    print("Election results:")
+    print(splitter)
+    for row in electionreader :
+        votes_cast.append(row[2])
+        votecount = len(votes_cast)
+    
+    print(f"Total number of votes: {votecount}")
+    print(splitter)
+
 
